2ff1abd0218cc6c642ea665602907aa32bfcf85a
youmanyousef <44135498+youmanyousef@users.noreply.github.com>
2018-10-14 04:34:40 -0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,1 +1,1 @@
-# hamdija-bot
+# TutorialBot
--- bot.js
+++ bot.js
@@ -1,107 +1,71 @@
 var Discord = require('discord.io');
 var logger = require('winston');
 var auth = require('./auth.json');
-var jArray = ['1','2','3']
-// Configure logger settings
-client.login(process.env.BOT_TOKEN);
+
+//Configure logger settings.
 logger.remove(logger.transports.Console);
-logger.add(new logger.transports.Console, {
-    colorize: true
+logger.add(logger.transports.Console, {
+    colorize:true
 });
+
 logger.level = 'debug';
-// Initialize Discord Bot
+
+//Initialize the bot
 var bot = new Discord.Client({
-   token: auth.token,
-   autorun: true
+    token: auth.token,
+    autorun:true
 });
-bot.on('ready', function (evt) {
+
+bot.on('ready', function(evt){
     logger.info('Connected');
     logger.info('Logged in as: ');
     logger.info(bot.username + ' - (' + bot.id + ')');
 });
-bot.on('message', function (user, userID, channelID, message, evt) {
-    // Our bot needs to know if it will execute a command
-    // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
+
+bot.on('message', function(user, userID, channelID, message, evt){
+    //Bot needs to know if it will execute a command.
+    //Bot will listen for messages that start with an '!'.
+    if(message.substring(0, 1) == '!'){
         var args = message.substring(1).split(' ');
         var cmd = args[0];
-       
+
         args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'h':
+        switch(cmd){
+            //!ping
+            case 'ping':
                 bot.sendMessage({
                     to: channelID,
-                    message: '`!c` - Counter Jays Win'
-                });
-				bot.sendMessage({
-                    to: channelID,
-                    message: '`!t` - Jays Win'
-                });
-				bot.sendMessage({
-                    to: channelID,
-                    message: '`!hieu` - hieu rOAST'
-                });
-				bot.sendMessage({
-                    to: channelID,
-                    message: '`!justin` - justin rOAST'
+                    message: "Hi, this is the tutorial bot!\nUse the command !tutcommands for all my commands!"
                 });
             break;
-			case 'justin':
-				var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-				if (randomJ == jArray[0]){
+            //!tutcommands
+            case 'tutcommands':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Justin got **BODIED** ok?'
-                });}
-				if (randomJ == jArray[1]){
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'If you dont stfu, ill body you like justin.'
-                });}
-				if (randomJ == jArray[2]){
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'You do know what I did to Justin, right?'
-                });}
-            break;
-            case 'c':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Counter-Jays Win!'
+                    message: "My commands are:\n!ping\n!tutcommands\n!talk\n!yell\n!calm"
                 });
             break;
-            case 't':
+            //!talk
+            case 'talk':
                 bot.sendMessage({
                     to: channelID,
-                    message: 'Jays Win!'
+                    message: "Look at me! I'm talking!"
                 });
             break;
-            // Just add any case commands if you want to..
-            case 'hieu':
-				var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-				if (randomJ == jArray[0])
+            //!yell
+            case 'yell':
                 bot.sendMessage({
                     to: channelID,
-                    message: '1-0, bodied that nigga.'
-                });
-				if (randomJ == jArray[1])
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'I made that bitch tap out:boxing_glove:.'
-                });
-				if (randomJ == jArray[2])
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'What a gay ass, he tried to pull down my pants.'
+                    message: "I'M YELLING OKAY!!"
                 });
             break;
-            case 'frederica':
+            //!calm
+            case 'calm':
                 bot.sendMessage({
-                    to: channelID,
-                    message: 'Ima marry her!'
+                        to: channelID,
+                        message: "Calm down, everything is going to be okay."
                 });
             break;
-         }
-     }
+        }
+    }
 });

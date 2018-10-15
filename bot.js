10abc56e39aedf7bec5d16cc45730ce44de8a9c0
youmanyousef <44135498+youmanyousef@users.noreply.github.com>
2018-10-14 17:18:16 -0700

    Update bot.js

--- bot.js
+++ bot.js
@@ -33,37 +33,7 @@ bot.on('message', function(user, userID, channelID, message, evt){
         switch(cmd){
             //help
             case 'h':
-			const embed = new Discord.RichEmbed()
-  .setTitle("This is your title, it can hold 256 characters")
-  .setAuthor("Author Name", "https://i.imgur.com/lm8s41J.png")
-  /*
-   * Alternatively, use "#00AE86", [0, 174, 134] or an integer number.
-   */
-  .setColor(0x00AE86)
-  .setDescription("This is the main body of text, it can hold 2048 characters.")
-  .setFooter("This is the footer text, it can hold 2048 characters", "http://i.imgur.com/w1vhFSR.png")
-  .setImage("http://i.imgur.com/yVpymuV.png")
-  .setThumbnail("http://i.imgur.com/p2qNFag.png")
-  /*
-   * Takes a Date object, defaults to current date.
-   */
-  .setTimestamp()
-  .setURL("https://discord.js.org/#/docs/main/indev/class/RichEmbed")
-  .addField("This is a field title, it can hold 256 characters",
-    "This is a field value, it can hold 2048 characters.")
-  /*
-   * Inline fields may not display as inline if the thumbnail and/or image is too big.
-   */
-  .addField("Inline Field", "They can also be inline.", true)
-  /*
-   * Blank field, useful to create some space.
-   */
-  .addBlankField(true)
-  .addField("Inline Field 3", "You can have a maximum of 25 fields.", true);
-               bot.sendMessage({
-	       		to: channelID,
-                    message: embed
-	       })
+			
             break;
             //Justin
 	    case 'justin':
@@ -92,50 +62,50 @@ bot.on('message', function(user, userID, channelID, message, evt){
                 });
             break;
             //ORANGE!
-	    //case 'og':
-		//var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		//if (randomJ == jArray[0]){
-                	//bot.sendMessage({
-                	    //	to: channelID,
-                    		//message: 'oraAANGe!'
-               		//});}
-		//if (randomJ == jArray[1]){
-		//	bot.sendMessage({
-               /// 	    	to: channelID,
-                 //   		message: ':large_orange_diamond::large_orange_diamond:!'
-               	//	});
-		//}
-		//if (randomJ == jArray[1]){
-		//	bot.sendMessage({
-              // 	    	to: channelID,
-              //      		message: 'OG! OG!'
-               //	/	});
-		//}
-	//	}
-            ///break;
+	    case 'og':
+		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+		if (randomJ == jArray[0]){
+                	bot.sendMessage({
+                	    	to: channelID,
+                    		message: 'oraAANGe!'
+               		});}
+		if (randomJ == jArray[1]){
+			bot.sendMessage({
+                	    	to: channelID,
+                    		message: ':large_orange_diamond::large_orange_diamond:!'
+               		});
+		}
+		if (randomJ == jArray[1]){
+			bot.sendMessage({
+               	    	to: channelID,
+                    		message: 'OG! OG!'
+               		});
+		}
+		}
+            break;
 			
             //Fortnite sayings
-	    //case 'fortnite':
-		//var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
-		//if (randomJ == jArray[0]){
-               	//	bot.sendMessage({
-                //    	to: channelID,
-                //    	message: 'Fuck fortnite, I just Play For Fun.'
-               // 	});
-		//}
-		//if (randomJ == jArray[1]){
-               //		bot.sendMessage({
-               //     	to: channelID,
-              //      	message: 'Incorrect, Gary. I have 23* solos wins.'
-              //  	});
-		//}
-		//if (randomJ == jArray[2]){
-              // 		bot.sendMessage({
-              //      	to: channelID,
-             //       	message: 'Only reason i have 1W% is because there noclippers, and hackers.'
-              //  	});
-		//}
-          // break;
+	    case 'fortnite':
+		var randomJ = jArray[Math.floor(Math.random()*jArray.length)];
+		if (randomJ == jArray[0]){
+               		bot.sendMessage({
+                  	to: channelID,
+                   	message: 'Fuck fortnite, I just Play For Fun.'
+                	});
+		}
+		if (randomJ == jArray[1]){
+               		bot.sendMessage({
+                    	to: channelID,
+                    	message: 'Incorrect, Gary. I have 23* solos wins.'
+                	});
+		}
+		if (randomJ == jArray[2]){
+               		bot.sendMessage({
+                    	to: channelID,
+                    	message: 'Only reason i have 1W% is because there noclippers, and hackers.'
+                	});
+		}
+          break;
             //Jays (t)
             case 't':
                 bot.sendMessage({
@@ -143,11 +113,11 @@ bot.on('message', function(user, userID, channelID, message, evt){
                     message: 'Jays Win!'
                 });
             break;
-	   // case 'hue':
-           //     bot.sendMessage({
-            //        to: channelID,
-            //        message: 'I buy hue KFC🍗!'
-             //   });
+	   case 'hue':
+                bot.sendMessage({
+                to: channelID,
+                message: 'I buy hue KFC🍗!'
+                });
             break;
             // hieu
             case 'hieu':
@@ -168,12 +138,12 @@ bot.on('message', function(user, userID, channelID, message, evt){
                     message: 'What a gay ass, he tried to pull down my pants.'
                 });
             break;
-            //case 'frederica':
-          //      bot.sendMessage({
-          //          to: channelID,
-         // //          message: 'Ima marry her!'
-         //       });
-         //   break;
+            case 'frederica':
+                bot.sendMessage({
+                    to: channelID,
+                    message: 'Ima marry her!'
+                });
+            break;
         }
     }
 });
